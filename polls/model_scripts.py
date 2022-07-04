7865ae1431554326230efa13298e3d6f27cab3f4
lishuai.jing <lishuai.jing@velux.com>
2022-07-04 08:38:14 +0200

    ex to unit 3

--- polls/model_scripts.py
+++ polls/model_scripts.py
@@ -6,5 +6,13 @@ from django.utils import timezone
 q = Question(question_text = 'who is nolan',pub_date = timezone.now())
 
 current_year = timezone.now().year
-question.objects.get(pub_date__year=current_year)
+Question.objects.get(pub_date__year=current_year)
  
+q = Question.objects.get(pk=4)
+
+q.choice_set.create(choice_text = 'superman',votes = 0)
+q.choice_set.create(choice_text = 'batman',votes = 0)
+q.choice_set.create(choice_text = 'antman',votes = 0)
+
+q.choice_set.all()
+q.choice_set.count()
